33a98fded8a0984615c94a60a48c0e2a7c3f7c32
sajid2405 <sajid2405@gmail.com>
2021-11-15 14:04:03 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require("express");
+
+var app = express();
+
+app.get('/', function(req, res){
+	res.send("Sajidullah Baig");
+});
+
+app.listen(3000);
+console.log("listening on port: 3000");
+
+module.exports = app;
